787221fde69438c5930af66c7662476922e539a9
fiona-chau <21chauf89@stu.smuhsd.org>
2018-08-22 11:24:25 -0700

    hello world commit today

---
+++ helloworld/Hello_World.java
@@ -0,0 +1,13 @@
+/**
+ * Write a description of class Hello_World here.
+ * 
+ * @author (Fiona) 
+ * @version (8/22/2018)
+ */
+public class Hello_World
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World! From Fiona"); //Display the string
+    }
+}
